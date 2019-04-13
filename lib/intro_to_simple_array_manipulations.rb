7fb0b5c7f1f791353fce9894d336c58762464175
Chris Kay <chriskay25@gmail.com>
2019-04-13 20:40:45 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, "string")
+  array.push("string")
+end
+
+def using_unshift(array, string)
+  
+end
+
+def using_pop(array)
+
+end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat(array_1, array_2)
+  
+end
+
+def using_insert(array, element)
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at
+  
+end
